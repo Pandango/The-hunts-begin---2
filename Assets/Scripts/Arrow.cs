62a9786eef8bf100b273892b69c94ea0561f5bf2
JackBerserk <powerunleash@gmail.com>
2016-04-26 18:53:01 +0700

    ใส่เสียง

--- Assets/Scripts/Arrow.cs
+++ Assets/Scripts/Arrow.cs
@@ -3,9 +3,14 @@ using System.Collections;
 
 public class Arrow : MonoBehaviour {
 
+AudioSource Shot,Fire;
+
 	// Use this for initialization
 	void Start () {
-	
+	 	AudioSource[] audios = GetComponents<AudioSource>();
+        Shot = audios[0];
+        Fire = audios[1];
+        Shot.Play();
 	}
 	
 	// Update is called once per frame
@@ -17,6 +22,7 @@ public class Arrow : MonoBehaviour {
     {
         if ((hitInfo.gameObject.tag != "Player") && (hitInfo.gameObject.tag != "Arrow") && (hitInfo.gameObject.tag != "ArrowSuper") && (hitInfo.gameObject.tag != "Slash"))
         {
+        	Fire.Play();
             Destroy(this.gameObject.GetComponent<Rigidbody>());
             Destroy(this.gameObject.GetComponent<Collider>());
         }
--- Assets/Scripts/ArrowStuck.cs
+++ Assets/Scripts/ArrowStuck.cs
@@ -3,12 +3,20 @@ using System.Collections;
 
 public class ArrowStuck : MonoBehaviour {
 
+	AudioSource Fire;
+
     float depth = 0.30f; // how deep the arrow will enter the body
+
+	void Start () {
+	 	AudioSource[] audios = GetComponents<AudioSource>();
+        Fire = audios[0];
+    }
 
     void OnCollisionEnter(Collision hitInfo)
     {
         if ((hitInfo.gameObject.tag == "Arrow") || (hitInfo.gameObject.tag == "ArrowSuper"))
         {
+        	Fire.Play();
             hitInfo.rigidbody.isKinematic = true; // stop physics control 
             hitInfo.transform.Translate(depth * Vector3.forward); // move the arrow deep inside 
             hitInfo.transform.parent = transform; // stuck the arrow to the enemy
